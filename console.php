17b4e8cff88442c19027201eed261ccbc6326aff
Hendrawan Kuncoro <pentatonicfunk@gmail.com>
2018-08-09 12:24:11 +0700

    define possible vendor path

--- console.php
+++ console.php
@@ -1,8 +1,16 @@
-#!/usr/bin/env php
 <?php
-// application.php
-
-require __DIR__.'/vendor/autoload.php';
+$possible_vendors = [
+    __DIR__.'/../../autoload.php',
+    __DIR__.'/vendor/autoload.php',
+];
+foreach ($possible_vendors as $file) {
+    if (file_exists($file)) {
+        /** @noinspection PhpIncludeInspection */
+        require $file;
+        $loaded = true;
+        break;
+    }
+}
 
 use Symfony\Component\Console\Application;
 
